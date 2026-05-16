54a7448136069f2b0a89536d57701b68c372e1cf
Paola Novick <paonovick10@gmail.com>
2026-05-16 17:58:25 -0300

    Integrate Clarity package and refine social metadata

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
+
+Clarity.init('ws5q3bzm8m')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
